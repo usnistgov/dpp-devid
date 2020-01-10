854dc9dbe0fdafb2799bedd79d332856eb8ba1fb
M. Ranganathan <mranga@nist.gov>
2020-01-10 13:09:35 -0500

    Fixed scripts to check for wpa running.

--- scripts/enrollee/start-enrollee.sh
+++ scripts/enrollee/start-enrollee.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+if [ -z ${PROJECT_HOME+x} ]; then
+        echo "PROJECT_HOME is unset"; exit
+else
+        echo "PROJECT_HOME is set to '$PROJECT_HOME'"
+fi
+pid=`pgrep wpa_supplicant`
+if [ -z ${pid+x} ]; then
+        echo "wpa_supplicant not started. Start it with start_wpas.sh"; exit
+else
+	echo "wpa_supplicant is running pid is '$pid'"
+fi
 sudo ifconfig wlan1 0
-cp wpa_supplicant.conf.orig wpa_supplicant.conf
 sudo python enrollee.py --if wlan1 --pkey $PROJECT_HOME/test/DevID50/DevIDSecrets/IDevID50.key.der --cf ./wpa_supplicant.conf
--- scripts/enrollee/start_wpas.sh
+++ scripts/enrollee/start_wpas.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-sudo pkill wpa_supplicant
+if [ -z ${PROJECT_HOME+x} ]; then
+        echo "PROJECT_HOME is unset"; exit
+else
+        echo "PROJECT_HOME is set to '$PROJECT_HOME'"
+fi
+
 sudo pkill wpa_supplicant
 sudo cp wpa_supplicant.conf.orig wpa_supplicant.conf
 sudo chown pi wpa_supplicant.conf
-sudo /usr/local/sbin/wpa_supplicant  -c`pwd`/wpa_supplicant.conf -iwlan1 -Dnl80211,wext -dd -f /tmp/debug.txt
+sudo /usr/local/sbin/wpa_supplicant  -c$PROJECT_HOME/scripts/enrollee/wpa_supplicant.conf -iwlan1 -Dnl80211,wext -dd -f /tmp/debug.txt
 
