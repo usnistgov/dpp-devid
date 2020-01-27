a81a39244e27ea1ff98f43e45bad970040eb96d6
M. Ranganathan <mranga@nist.gov>
2020-01-26 21:57:14 -0500

    Fixed start script.

--- scripts/configurator/start-wpas.sh
+++ scripts/configurator/start-wpas.sh
@@ -4,6 +4,7 @@ if [ -z ${PROJECT_HOME+x} ]; then
 else
 	echo "PROJECT_HOME is set to '$PROJECT_HOME'"
 fi
+sudo rm -f /tmp/debug.txt
 sudo pkill wpa_supplicant
 echo "Starting wpa_supplicant"
 sudo /usr/local/sbin/wpa_supplicant  -c$PROJECT_HOME/scripts/configurator/wpa_supplicant.conf -iwlan1 -Dnl80211,wext -dd -f /tmp/debug.txt
